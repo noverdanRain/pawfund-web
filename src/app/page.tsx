0197b20e3bacbce74240ed414ea964a1ce41b3fa
Galang Arsandy <galangarsandy@gmail.com>
2025-10-11 03:25:35 +0700

    feat: update Home component to display welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <div className="min-h-screen p-8 pb-20 font-sans sm:p-20"></div>;
+	return (
+		<div className="min-h-screen p-8 pb-20 font-sans sm:p-20">
+			<h1 className="text-3xl font-bold">Welcome to PawFund!</h1>
+		</div>
+	);
 }
