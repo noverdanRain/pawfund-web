10402b4cb503d1a075731eb9259514e04fa3d6cc
Galang Arsandy <galangarsandy@gmail.com>
2025-10-19 01:38:10 +0700

    fix: update import path for cookieStorage and createStorage from wagmi

--- src/config/reownConfig.ts
+++ src/config/reownConfig.ts
@@ -1,4 +1,4 @@
-import { cookieStorage, createStorage } from "@wagmi/core";
+import { cookieStorage, createStorage } from "wagmi";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { sepolia } from "@reown/appkit/networks";
 import { REOWN_PROJECT_ID } from "./envVars";
